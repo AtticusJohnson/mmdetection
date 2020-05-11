6f725fdfcf184615286071e7a0563956801d4309
Atticus Johnson <30431995+AtticusJohnson@users.noreply.github.com>
2020-05-11 17:55:24 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements/build.txt
+pip install "git+https://github.com/cocodataset/cocoapi.git#subdirectory=PythonAPI"
+pip install -v -e .
